e5eee425f45107b63732aa38712f96d612621cf4
Andrew Oispov <mail@andrew-osipov.pro>
2021-06-22 09:36:36 +0500

    git init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import FormCreator from '../FormCreator'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div></div>
+      <FormCreator />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormCreator/types.ts
@@ -0,0 +1,5 @@
+export interface IField {}
+
+export interface IFormCreatorProps {
+  fields: IField
+}
